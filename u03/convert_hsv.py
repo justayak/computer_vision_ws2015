7d98898f4f6be08b0e7b773fdbd51faaa2acee8a
julian <jutanke@googlemail.com>
2015-11-11 21:23:43 +0100

    start hsv conversion

--- u03/convert_hsv.py
+++ u03/convert_hsv.py
@@ -28,8 +28,17 @@ Cmax = stacked.max(2)
 Cmin = stacked.min(2)
 Delta = Cmax - Cmin
 
-print(Delta)
+RMask = 1 * (R == Cmax)
+GMask = 1 * (G == Cmax)
+BMask = 1 * (B == Cmax)
 
+# make sure there is no overlapping:
+R_G_eq = np.transpose(np.nonzero(1 * (RMask == GMask)))
+
+
+print(R_G_eq)
+print(RMask)
+print(GMask)
 
 
 #plt.imshow(Idash)
