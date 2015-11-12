a7c662ad234581046e4989d86e1d1d76550802dc
julian <jutanke@googlemail.com>
2015-11-12 21:38:30 +0100

    task 2

--- u03/camshift.py
+++ u03/camshift.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import ndimage
+import cv2
 from matplotlib import pyplot as plt, colors
 
 def camshift(F, Histo):
@@ -12,17 +13,50 @@ def camshift(F, Histo):
     HSV = colors.rgb_to_hsv(F)
     H = np.zeros((HSV.shape[0], HSV.shape[1]), dtype=HSV.dtype)
     H[:, :] = HSV[:, :, 0]
-    P = np.zeros((HSV.shape[0], HSV.shape[1]), dtype=HSV.dtype)
+    H *= (len(Histo) - 1)  # normalize for array access
+    H = H.astype(int)
+    P = np.zeros((HSV.shape[0], HSV.shape[1]), dtype=np.float16)
+    h = H.shape[0]
+    w = H.shape[1]
+    for x in range(0, w):
+        for y in range(0, h):
+            P[y][x] = Histo[H[y][x]]
     return P
 
-# Calculate CAR histogram
-I = ndimage.imread("racecar.png")
-I = I[260:360, 460:660]
+def histo(I):
+    """
+    Calculates the probability histogram
+    :param I:
+    :return: 1d probability histogram with 255 elements
+    """
+    HSV = colors.rgb_to_hsv(I)
+    H = np.zeros((HSV.shape[0], HSV.shape[1]), dtype=HSV.dtype)
+    H[:, :] = HSV[:, :, 0]
+    hist, bins = np.histogram(H.flatten("C"), bins=255, range=(0.0, 1.0))
+    total = H.shape[0] * H.shape[1]
+    return hist/total
 
-HSV = colors.rgb_to_hsv(I)
-H = np.zeros((HSV.shape[0], HSV.shape[1]), dtype=HSV.dtype)
-H[:, :] = HSV[:, :, 0]
+def get_nth_frame(video_file, n):
+    cam = cv2.VideoCapture(video_file)
+    if cam.isOpened():
+        for x in range(0, n+1):
+            _, img = cam.read()
+            if x == n:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                return img
 
-BLUE_CAR_HISTO, bins = np.histogram(H.flatten("C"), bins=255, range=(0.0, 1.0))
+def main():
+    # Calculate CAR histogram
+    I = ndimage.imread("racecar.png")
+    Car = I[270:350, 480:640]
+    #Car = I[260:360, 460:660]
+    F = get_nth_frame("racecar.avi", 300)
+    BLUE_CAR_HISTO = histo(Car)
+    P = camshift(F, BLUE_CAR_HISTO)
 
-# apply histogram
+    plt.imshow(P, cmap='gray', vmin=0, vmax=0.2)
+    #plt.imshow(Car)
+    plt.show()
+    # apply histogram
+
+main()
