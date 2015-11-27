526ff239655331adb63c54b9d5e54471de19b2e8
julian <jutanke@googlemail.com>
2015-11-27 15:25:06 +0100

    restruct

---
+++ u04/task1.py
@@ -0,0 +1,6 @@
+# Aufgabe 1
+# (Fortsetzung der Aufgabe 2 des vorangeganenen Übungsblattes)
+# implementiere die Berechnung des Fensterzentrums, -größe und Objektorientierung wie im Paper
+# tracke das Auto vom ersten zum letzten Frame
+# Abgabe 1.1 : letztes Einzelbild im Video plus Overlay des Suchfensters
+
