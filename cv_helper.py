e49197fc88b84c5960c21e61e2cb616b1437932f
julian <jutanke@googlemail.com>
2015-11-27 16:47:44 +0100

    show rect works!

--- cv_helper.py
+++ cv_helper.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
-import numpy as np
 import cv2
+import numpy as np
 
 
 def load_video_as_rgb(video_file):
@@ -14,21 +14,41 @@ def load_video_as_rgb(video_file):
     if cam.isOpened():
         ret, img = cam.read()
         stream.append(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        #stream.append(img)
         while ret:
             ret, img = cam.read()
             if ret:
                 stream.append(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-                #stream.append(img)
     return stream
 
 
-def play_video(video):
-    frame = video[0]
+def play_video(video, overlay=None):
+    """
+    Plays a list of rgb images
+    careful, performance is not key here!
+    :param video: List of rgb images
+    :param overlay: List of tuple for drawing rect per frame
+            (x, y, w, h)
+    :return:
+    """
+    if overlay is not None:
+        assert(len(video) == len(overlay))
+
+    frame = video[0]  # i dont care for the first overlay frame..
     frames = len(video)
     plt.axis("off")
-    im = plt.imshow(frame)
+    RED = np.array([255, 0, 0])
+    LW = 6
+
+    ax = plt.gca()
+    im = ax.imshow(frame)
     for i in range(1, frames):
-        im.set_data(video[i])
+        frame = video[i]
+        if overlay is not None:
+            (x, y, w, h) = overlay[i]
+            frame[y:y+h, x-LW:x] = RED
+            frame[y:y+h, x+w:x+w+LW] = RED
+            frame[y+h:y+h+LW, x:x+w] = RED
+            frame[y-LW:y, x:x+w] = RED
+        im.set_data(frame)
         plt.pause(0.02)
     plt.show()
