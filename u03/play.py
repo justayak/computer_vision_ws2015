0691bccf5a109553371bc27b4a4bb1195d51d5ab
julian <jutanke@googlemail.com>
2016-01-16 12:55:14 +0100

    play around

--- u03/play.py
+++ u03/play.py
@@ -1,20 +1,21 @@
 from scipy import signal, ndimage
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.cm as cm
 
 I = ndimage.imread("racecar.png")
 
+Kx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 I = rgb2gray(I)
-Sx = ndimage.sobel(I, 0)
+#Sx = ndimage.sobel(I, 0)
+Sx = signal.convolve2d(I, Kx, boundary='symm', mode='same')
 
-print(Sx.shape)
+#F = np.zeros((Sx.shape[0], Sx.shape[1], 3))
+#F[...,:3] = Sx.reshape((Sx.shape[0], Sx.shape[1], 1))
 
-F = np.zeros((Sx.shape[0], Sx.shape[1], 3))
-F[...,:3] = Sx.reshape((Sx.shape[0], Sx.shape[1], 1))
-
-plt.imshow(F)
+plt.imshow(Sx, cmap=cm.Greys_r)
 plt.show()
