96d1442368735255a2bdf9b94ba29790d0640f4d
julian <jutanke@googlemail.com>
2016-02-01 17:21:22 +0100

    better print Mat and start with canny

--- juvis.py
+++ juvis.py
@@ -112,6 +112,51 @@ def plot_vec(u, v):
     plt.ylim(-1, height)
     plt.show()
 
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    dtype = value.dtype
+    leftSpan = abs(float(leftMax - leftMin))
+    rightSpan = abs(float(rightMax - rightMin))
+    value -= leftMin
+    value = value / leftSpan
+    value *= rightSpan
+    return value.astype(dtype)
+
+
+def plot_mats(mats, cols=5, cmap=plt.get_cmap('gray')):
+    SUBSTITUTE = np.zeros_like(mats[0])
+    rows = []
+    currentRow = []
+    cols = float(cols)
+
+    def add_to_rows():
+        while len(currentRow) < cols:
+            currentRow.append(SUBSTITUTE)
+        rows.append(np.hstack(currentRow))
+    
+    for i in range(0, len(mats)):
+        M = mats[i]
+        minv = np.min(M)
+        maxv = np.max(M)
+        if minv < 0 or minv > 255 or maxv < 0 or maxv > 255:
+            M = translate(M, minv, maxv, 0, 255)
+
+        if i%cols == 0:
+            if len(currentRow) > 0:
+                add_to_rows()
+            currentRow = []
+        currentRow.append(mats[i])
+    if len(currentRow) > 0:
+        add_to_rows()
+    I = np.vstack(rows)
+    f, ax = plt.subplots(ncols=1)
+    f.set_size_inches(16,16)
+    ax.imshow(I, cmap=cmap)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+
 def paint_mats(mats, interpolation='bilinear', vmin=None, vmax=None, max_row=10):
     """
     @mats {Matrices}: images to draw
